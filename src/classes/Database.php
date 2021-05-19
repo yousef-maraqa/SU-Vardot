33305162b2e0159b47ee471b3a417bb4049e4cfd
yousef-maraqa <yousefmaraqa98@gmail.com>
2021-05-19 16:43:36 +0300

    php test

---
+++ src/classes/Database.php
@@ -0,0 +1,31 @@
+<?php  
+class Database{
+    private $host ='localhost';
+    private $user ='root';
+    private $pass ='123123';
+    private $dbname ='su';
+    private $dbh;
+    private $error;
+    private $stmt;
+
+    public function __construct(){
+		// Set DSN
+		$dsn = 'mysql:host='. $this->host . ';dbname='. $this->dbname;
+		// Set Options
+		$options = array(
+			PDO::ATTR_PERSISTENT => true,
+			PDO::ATTR_ERRMODE	 => PDO::ERRMODE_EXCEPTION
+		);
+		// Create new PDO
+		try {
+			$this->dbh = new PDO($dsn, $this->user, $this->pass, $options);
+		} catch(PDOEception $e){
+			$this->error = $e->getMessage();
+		}
+	}
+}
+
+     
+
+
+?>
